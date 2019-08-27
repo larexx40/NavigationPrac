a06bfcdcc003d42c8a069e72adb4a74f5e606da2
root <root@localhost.localdomain>
2019-08-27 19:47:48 +0000

    Initial commit

---
+++ app/src/main/java/com/example/navigationprac/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.navigationprac
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        navController = Navigation.findNavController(this, R.id.fragment)
+        bottomNav.setupWithNavController(navController)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return  NavigationUI.navigateUp(navController, null)
+    }
+}
